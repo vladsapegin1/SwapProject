b67887091469705cc10ac388d8e9cd8f39f84a27
User <User@K3-512-11>
2017-12-06 19:25:33 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication17/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 1;
+
+            SwapInt(ref a, ref b);
+
+            Console.WriteLine("a={0}, b={1}", a, b);
+        }
+
+        private static void SwapInt(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+    }
+}
